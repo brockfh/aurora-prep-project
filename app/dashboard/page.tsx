96d99e5d176a4196ffa28f663a683454eb2f5b20
Brock Balducci <tbalducci@firsthorizon.com>
2025-02-20 13:10:51 -0600

    set up project and create one server and one client component

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import {Button} from '../components/Button' //named export
+// import 'Button' from '../component/Button' // default export
+
+export default function Dashboard(){
+    return (
+        <>
+            <div>
+            Hello from the dashboard component
+            </div>
+            <Button/>
+        </>
+    )
+}
