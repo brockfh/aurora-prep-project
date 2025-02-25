e114f492f314a9a7dfb68f5aaa03dcec3216b505
Brock Balducci <tbalducci@firsthorizon.com>
2025-02-25 13:36:26 -0600

    introduce useState and useEffect hooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { EffectComponent } from "./components/EffectComponent";
+import State from "./state/page";
+
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div className="p-8">
+      <h1>Homepage</h1>
+      <EffectComponent />
+    </div>
   );
 }
