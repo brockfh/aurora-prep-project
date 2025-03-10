d81aed7e3f5d38ed08feffdf479a8db7d0fa9ba2
Brock Balducci <tbalducci@firsthorizon.com>
2025-03-10 13:15:43 -0500

    set up prisma orm

--- app/lib/helper.ts
+++ app/lib/helper.ts
@@ -2,7 +2,7 @@ export const formatDate = (dateString: string) => {
   // destructure the day, month and year from the dateString
   const [day, month, year] = dateString.split("-");
   // create a new Date object
-  const date = new Date(year, month, day); // month is zero indexed
+  const date = new Date(year, month - 1, day); // month is zero indexed
   // Format the date to "Month Day, Year"
   const options = { year: "numeric", month: "long", day: "numeric" };
   return date.toLocaleDateString("en-us", options);
